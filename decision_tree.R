898dc69897ca9cdd1cd73c0c5e817f00bde93a5c
Elvio Gomes <elviobg@gmail.com>
2019-04-12 14:35:35 -0300

    Implements gain function

--- decision_tree.R
+++ decision_tree.R
@@ -3,6 +3,21 @@ entropy <- function(nPositive, nNegative) {
   -nPositive/t * log2(nPositive/t) -nNegative/t * log2(nNegative/t);
 }
 
+gain <- function(entropy, data, labels) {
+  
+  c <- data.frame(value=data, label=labels)
+  s <- length(labels)
+  e <- c()
+  g <- entropy
+  
+  for (level in levels(data)) {
+    t <- which(c$value == level & c$label== 'TRUE')
+    f <- which(c$value == level & c$label== 'FALSE')
+    g = g - entropy(length(f), length(t)) * (length(f) + length(t)) / s
+  }
+  return(g)
+}
+
 get_data <- function() {
   #0.94
   tempo = c('e','e','n','c','c','c','n','e','e','c','e','n','n','c');
